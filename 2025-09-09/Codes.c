42be0821106db8042211a4cfe4fbb94fcc0ae277
Minseong Choi <me@flyemoji.moe>
2025-09-09 14:22:10 +0900

    chore!: Code Changed.

--- 2025-09-09/Codes.c
+++ 2025-09-09/Codes.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
 int main() {
-    printf("Hello, World!");
+    printf("123\n45\n"); // using 2 new lines to print 2 numbers
+    printf("C Programming");
+    return 0;
+/*  
+    교수님
+    재발
+    수업
+    끝내주세요
+    졸려요
+    ㅅㅂ
+*/
 }
