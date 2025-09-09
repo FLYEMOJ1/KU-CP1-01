d4da1ab4559224b479f6212a6c8223c266d7f6f8
Minseong Choi <me@flyemoji.moe>
2025-09-09 14:32:14 +0900

    chore!: masking the Day 1 codes.

--- 2025-09-09/Codes.c
+++ 2025-09-09/Codes.c
@@ -1,14 +1,17 @@
+/*
+Day 1 Codes
+
 #include <stdio.h>
 int main() {
     printf("123\n45\n"); // using 2 new lines to print 2 numbers
     printf("C Programming");
     return 0;
-/*  
+  
     교수님
     재발
     수업
     끝내주세요
     졸려요
     ㅅㅂ
-*/
 }
+*/
