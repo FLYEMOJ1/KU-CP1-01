77b34350624bd288e997d5141108a8b66c03ce53
Minseong "FLYEMOJI" Choi <me@flyemoji.moe>
2025-11-24 18:16:14 +0900

    !new: Added Homework - pointer programming

---
+++ Homework/pointer_Programming/result_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[2][3];
+    int (*p)[3] = matrix;
+    int val = 1;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] = val++;
+        }
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", p[i][j]);
+            count++;
+            if (count % 2 == 0) {
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
