af437404885bf2dc878b69ca777f37609ac54845
Minseong "FLYEMOJI" Choi <me@flyemoji.moe>
2025-12-09 10:11:17 +0900

    chore!: Factorial Calculation

--- 2025-12-09/a.c
+++ 2025-12-09/a.c
@@ -1,2 +1,24 @@
+// Factorial Calculation
+
 #include <stdio.h>
 
+fact (int num){
+    int sum = 1;
+    if (num == 0 || num == 1)
+        return 1;
+    else
+        for(int i = 1; i <= num; i++){
+            sum *= i;
+        }
+    return sum;
+}
+
+int main() {
+    int result, inN;
+    printf("Factorial Calc: ");
+    scanf("%d", &inN);
+
+    result = fact(inN);
+    printf("%d\n", result);
+    return 0;
+}
